1fbcca052d6d6b5c778e6b594288566c1f455866
buckwheat <comp322012016@mail.ru>
2016-04-01 16:18:30 +0300

    myProject

---
+++ PDO.php
@@ -0,0 +1,21 @@
+﻿<meta charset ="utf-8">
+<?php
+$host='localhost';
+$dbname='spisok';
+$user='root';
+ $password='';
+try {
+  $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->exec("set names utf8");
+}
+catch(PDOException $e) {
+    echo $e->getMessage();
+}
+$stmt = $db->query('SELECT * FROM lk7;');
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+foreach ($stmt as $row)
+{
+    echo "<p>".  $row['Name']. "&nbsp;" . $row['Fname']."</p>";
+}
+?>
